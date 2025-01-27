25aa49059ac0da30cddb88701d879cddbc218a2a
K. Papadopoulos <kostashope@hotmail.com>
2025-01-27 20:59:46 +0200

    update

--- src/gr/aueb/cf/mobilecontacts/model/dao/MobileContactDAOImpl.java
+++ src/gr/aueb/cf/mobilecontacts/model/dao/MobileContactDAOImpl.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 public class MobileContactDAOImpl implements IMobileContactDAO{
     private static final List<MobileContact> contacts = new ArrayList<>();
+    private static Long id = 1L;
 
     @Override
     public MobileContact insert(MobileContact mobileContact) {
+        mobileContact.setId(id++);
         contacts.add(mobileContact);
         return mobileContact;
     }
@@ -22,43 +24,59 @@ public class MobileContactDAOImpl implements IMobileContactDAO{
 
     @Override
     public void deleteById(Long id) {
-
+//        contacts.remove(getIndexById(id));
+        contacts.removeIf(contact -> contact.getId().equals(id));
     }
 
     @Override
     public MobileContact getById(Long id) {
-        return null;
+        int positionToReturn = getIndexById(id);
+        return (positionToReturn != -1) ? contacts.get(positionToReturn) : null;
     }
 
     @Override
     public List<MobileContact> getAll() {
-        return List.of();
+        return new ArrayList<>(contacts);
     }
 
     @Override
     public void deleteByPhoneNumber(String phoneNumber) {
-
+        contacts.removeIf(contact -> contact.getPhoneNumber().equals(phoneNumber));
     }
 
     @Override
     public MobileContact getByPhoneNumber(String phoneNumber) {
-        return null;
+        int positionToReturn = getIndexByPhoneNumber(phoneNumber);
+        return (positionToReturn != -1) ? contacts.get(positionToReturn) : null;
     }
 
     @Override
     public boolean userIdExists(Long id) {
-        return false;
+        int position = getIndexById(id);
+        return position != -1;
     }
 
     @Override
     public boolean phoneNumber(String phoneNumber) {
-        return false;
+        int position = getIndexByPhoneNumber(phoneNumber);
+        return position != -1;
     }
 
     private int getIndexById(Long id) {
         int positionToReturn = -1;
         for (int i = 0; i < contacts.size(); i++) {
             if (contacts.get(i).getId().equals(id)) {
+                positionToReturn = i;
+                break;
+            }
+        }
+        return positionToReturn;
+    }
+
+    private int getIndexByPhoneNumber(String phoneNumber) {
+        int positionToReturn = -1;
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getPhoneNumber().equals(phoneNumber)) {
                 positionToReturn = i;
                 break;
             }
